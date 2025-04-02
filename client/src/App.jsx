4213e7ea151becc5b85e9cb2e957f65fdd5d55a9
Tollhouse <mistertollhouse@gmail.com>
2025-04-02 10:14:02 -0600

    Massive rollback because I cant do things correctly STABLE point to return to that doesnt break everything

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 
 function App() {
   const [items, setItems] = useState([]);
-  const [user, setUser] = useState('');
+  const [user, setUser] = useState([]);
 
   useEffect(() => {
     fetch('http://localhost:3000/item')
@@ -13,17 +13,28 @@ function App() {
       return res.json();
     })
     .then((data) => {
-      console.log(data)
       setItems(data);
     })
   }, []);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/users')
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      setUser(data);
+      console.log(user)
+    })
+  }, []);
+
   return (
     <>
       <div>
         Inventory Management
       </div>
       <div className = "buttonlist">
-        {items.map(item => <button>{item.itemname}</button>)}
+        {items.map(item => <button>{item.itemname} Quantity:{item.quantity} <br/> Added by: {user[0].username}</button>)}
       </div>
     </>
   )
--- server/src/server.js
+++ server/src/server.js
@@ -12,28 +12,41 @@ server.get('/', async (req, res) => {
 })
 
 server.get('/users', async (req, res) => {
-    knex('users')
-    .select('*')
-    .then(data => {
-        res.status(200).json(data)
-    });
+    try{
+        const data = await knex('users').select('*');
+        res.status(200).json(data);
+    } catch(error){
+        res.status(500).json({message: "Error retrieving data.", error})
+    }
 });
 
-// server.post('/users', async (req, res) =>{
-//     knex('user')
-//     .insert()
-// })
+server.post('/users', async (req, res) =>{
+    const {input} = req.body;
+    try{
+    knex('users')
+    .insert(input)
+    } catch(error){
+        res.status(500).json({message: "Internal Server Error Inserting Data.", error})
+    }
+})
 
 // server.patch('/users', async (req, res) =>{
 
 // });
 
-// server.delete('/users', async (req, res) => {
-
-// });
+server.delete('/users', async (req, res) => {
+    const {id} = req.body.id;
+    try{
+    knex('users')
+    .where(id, id)
+    .del()
+    } catch(error){
+        res.status(500).json({message: "Could Not Delete User.", error})
+    }
+});
 
 server.get('/item', async (req, res) => {
-    try {
+    try{
         const data = await knex('item').select('*');
         res.status(200).json(data);
     } catch(error){
